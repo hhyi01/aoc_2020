20e4dab4deb91b13725897e70f3fc862a0ea925a
hhyi01 <helen.yi@moodys.com>
2020-12-08 18:24:38 -0800

    submit Day 8 part 2

--- day08_part1_part2.py
+++ day08_part1_part2.py
@@ -2,43 +2,66 @@
 with open("input_08.txt") as f:
     content = f.readlines()
 
-instructions = [x.strip() for x in content]
+instruction_list = [x.strip() for x in content]
 
-accumulator = 0
-executed_instructions = set()
-index = 0
 
-while index < len(instructions):
-    instruction = instructions[index].split(" ")
-    operation = instruction[0]
-    argument = int(instruction[1])
-    print("Operation: " + operation + ", Argument: " + str(argument))
-    if operation == "nop":
-        if index not in executed_instructions:
-            executed_instructions.add(index)
-        else:
-            print("Repeated instruction: " + str(index))
+# part 1
+def run_instructions(instructions, index):
+    accumulator = 0
+    executed_instructions = set()
+    repeated_instruction = False
+
+    while index < len(instructions):
+        instruction = instructions[index].split(" ")
+        operation = instruction[0]
+        argument = int(instruction[1])
+        # print("Operation: " + operation + ", Argument: " + str(argument))
+        if operation == "nop":
+            if index not in executed_instructions:
+                executed_instructions.add(index)
+            else:
+                # print("Repeated instruction: " + str(index))
+                repeated_instruction = True
+                break
+            index += 1
+        if operation == "acc":
+            if index not in executed_instructions:
+                executed_instructions.add(index)
+            else:
+                # print("Repeated instruction: " + str(index))
+                repeated_instruction = True
+                break
+            accumulator += argument
+            index += 1
+        if operation == "jmp":
+            if index not in executed_instructions:
+                executed_instructions.add(index)
+            else:
+                # print("Repeated instruction: " + str(index))
+                repeated_instruction = True
+                break
+            index += argument
+    return accumulator, repeated_instruction
+
+
+# print("Value of accumulator - part 1: " + str(run_instructions(instruction_list, 0)))
+
+
+# part 2
+# find jmp operation that should be nop or vice versa
+
+for idx, inst in enumerate(instruction_list):
+    if "nop" in instruction_list[idx]:
+        instruction_list_copy = instruction_list[:]
+        instruction_list_copy[idx] = instruction_list_copy[idx].replace("nop", "jmp")
+        results_nop = run_instructions(instruction_list_copy, 0)
+        if not results_nop[1]:
+            print("Result tuple: " + str(results_nop))
             break
-        index += 1
-    if operation == "acc":
-        if index not in executed_instructions:
-            executed_instructions.add(index)
-        else:
-            print("Repeated instruction: " + str(index))
+    if "jmp" in instruction_list[idx]:
+        instruction_list_copy = instruction_list[:]
+        instruction_list_copy[idx] = instruction_list_copy[idx].replace("jmp", "nop")
+        results_jmp = run_instructions(instruction_list_copy, 0)
+        if not results_jmp[1]:
+            print("Result tuple: " + str(results_jmp))
             break
-        accumulator += argument
-        index += 1
-    if operation == "jmp":
-        if index not in executed_instructions:
-            executed_instructions.add(index)
-        else:
-            print("Repeated instruction: " + str(index))
-            break
-        index += argument
-
-
-print("Value of accumulator: " + str(accumulator))
-
-
-
-
