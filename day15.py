27455290ab4b797470d68db4ef221f9d0d8325fa
hhyi01 <helen.yi@moodys.com>
2020-12-15 18:29:27 -0800

    submit Day 15 part 2 solution

--- day15.py
+++ day15.py
@@ -10,6 +10,7 @@ def start_game(start_nums, rounds):
     print("Starting numbers: " + str(spoken_nums))
     while len(spoken_nums) < rounds:
         last_num = spoken_nums[0]
+        print("Last number: " + str(last_num))
         last_spoken_nums = spoken_nums[1:]
         if last_num in last_spoken_nums:
             last_two = []
@@ -26,5 +27,54 @@ def start_game(start_nums, rounds):
     return spoken_nums[0]
 
 
-print(start_game(starting_numbers, 2020))
+# print(start_game(starting_numbers, 2020))
+
+
+# part 2
+def populate_start_nums_cache(start_nums):
+    start_nums_cache = {}
+    for idx, val in enumerate(start_nums):
+        start_nums_cache[val] = [idx]
+    return start_nums_cache
+
+
+def start_game_v2(start_nums, rounds):
+    curr_position = len(start_nums)
+    spoken_nums = populate_start_nums_cache(start_nums)
+    print("Starting numbers: " + str(spoken_nums))
+    last_number = start_nums[-1]
+    print("First last number: " + str(last_number))
+    while curr_position < rounds:
+        if curr_position % 10000 == 0:
+            print("Current position: " + str(curr_position))
+        if last_number in spoken_nums:
+            if len(spoken_nums[last_number]) > 1:
+                last = spoken_nums[last_number][-1]
+                prev = spoken_nums[last_number][-2]
+                next_number = last - prev
+                if next_number in spoken_nums:
+                    spoken_nums[next_number].append(curr_position)
+                else:
+                    spoken_nums[next_number] = [curr_position]
+                last_number = next_number
+            else:
+                next_number = 0
+                if next_number in spoken_nums:
+                    spoken_nums[next_number].append(curr_position)
+                else:
+                    spoken_nums[next_number] = [curr_position]
+                last_number = next_number
+        else:
+            spoken_nums[last_number] = [curr_position]
+            next_number = 0
+            if next_number in spoken_nums:
+                spoken_nums[next_number].append(curr_position)
+            else:
+                spoken_nums[next_number] = [curr_position]
+            last_number = next_number
+        curr_position += 1
+    return last_number
+
+
+print(start_game_v2(starting_numbers, 30000000))
 
